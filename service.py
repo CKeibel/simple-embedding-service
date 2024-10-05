5f381e50dce4587c4bae48af3f07f8aa5717fd00
Christopher Keibel <christopher.keibel.90@gmail.com>
2024-10-05 19:20:29 +0200

    implement vector service

---
+++ service.py
@@ -0,0 +1,22 @@
+from models.factory import VectorizerFactory
+
+
+class SingeltonMeta(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+
+
+class VectorService(type=SingeltonMeta):
+    def __init__(self, model_id: str) -> None:
+        self.model = VectorizerFactory.get_model(model_id)
+
+    def vectorize(self, inputs: str | list[str]) -> list[list[float]]:
+        return self.model.vectorize(inputs)
+
+    def create_service(cls, model_id: str):
+        return cls(model_id)
